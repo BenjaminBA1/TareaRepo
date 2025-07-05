8c4ef8c21adb173f7b40a4cc2f6b356b16a453d5
benal <benal@PC_Ben>
2025-07-04 20:46:57 -0600

    Segundo Commit (Se agregó un codigo simple en el main)

--- src/tarearepo/TareaREPO.java
+++ src/tarearepo/TareaREPO.java
@@ -4,6 +4,8 @@
  */
 package tarearepo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author benal
@@ -13,8 +15,32 @@ public class TareaREPO {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Introducción
+        System.out.println("========================================");
+        System.out.println("    Bienvenido al programa Contador ");
+        System.out.println("   Este programa te pedira un numero");
+        System.out.println("y luego contara desde 1 hasta ese numero.");
+        System.out.println("=========================================\n");
+
+        // Entrada del usuario
+        System.out.print("Por favor, ingresa un numero entero positivo: ");
+        int numero = scanner.nextInt();
+
+        // Validación simple
+        if (numero <= 0) {
+            System.out.println("El numero debe ser mayor que cero.");
+        } else {
+            // Contador
+            System.out.println("\nContando del 1 al " + numero + ":");
+            for (int i = 1; i <= numero; i++) {
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
     }
     
 }
